a522c71422ea34eea51b907f4a0f96da784cf207
tred-07 <aranna.rtx.bd.02@gmail.com>
2025-01-22 21:29:07 +0600

    upd

--- app.js/app.js
+++ app.js/app.js
@@ -82,11 +82,10 @@ const showAllAdvertiseButNoAction = () => {
   fetch("https://qrent-backend.onrender.com/advertise/all/")
     .then(res => res.json())
     .then(data => {
-      console.log(data);
       data.forEach(ad => {
         const parent = document.getElementById("all-advertise")
         const div = document.createElement("div")
-        if (ad.image != null && ad.is_approved!=false) {
+        if (ad.image != null && ad.is_approved != false) {
           div.innerHTML = `
           <img src="${ad.image}">
           <h2 class="text-center font-bold text-[1.25rem]">${ad.title}</h2>
@@ -95,9 +94,8 @@ const showAllAdvertiseButNoAction = () => {
           <button onclick="showAdDetails(${ad.id})" class="btn btn-primary w-[50%] mx-auto my-[25px]">Show Details</button>`
           div.classList.add('flex', 'flex-col', 'justify-center', 'border-[black]', 'rounded-[25px]', 'px-[20px]', 'py-[20px', 'border-[5px]', 'w-[20%]')
           parent.appendChild(div)
-
         }
-        else if(ad.is_approved!=false) {
+        else if (ad.is_approved != false) {
           div.innerHTML = `
           <img src="pngtree-no-image-available-icon-flatvector-illustration-pic-design-profile-vector-png-image_40966566.jpg">
           <h2 class="text-center font-bold text-[1.25rem]">${ad.title}</h2>
@@ -106,11 +104,8 @@ const showAllAdvertiseButNoAction = () => {
           <button onclick="showAdDetails(${ad.id})" class="btn btn-primary w-[50%] mx-auto my-[25px]">Show Details</button>`
           div.classList.add('flex', 'flex-col', 'justify-center', 'border-[black]', 'rounded-[25px]', 'px-[20px]', 'py-[20px', 'border-[5px]', 'w-[20%]')
           parent.appendChild(div)
-
         }
-        console.log(ad.title);
-        console.log(ad.description);
-        console.log(ad.price);
+
         ad.review.forEach(re => {
           console.log(re.user);
           console.log(re.star);
