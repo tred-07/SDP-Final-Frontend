42c8622aaf7b5c3110a4625eb9b611a438472515
tred-07 <aranna.rtx.bd.02@gmail.com>
2025-01-23 00:53:41 +0600

    upd

--- profile.js
+++ profile.js
@@ -11,7 +11,17 @@ const fetchProfile=()=>{
     ).
     then(response => response.json())
     .then(data => {
-        console.log(data);
+        data.forEach(element => {
+            const parent=document.getElementById("profile-container")
+            const div=document.createElement("div")
+            div.innerHTML=`
+            <h2>Username: ${element.username}</h2>
+            <p>${element.first_name} ${element.last_name}</p>
+            <p>${element.email}</p>
+            `
+            div.classList.add('flex', 'flex-col', 'justify-center', 'border-[black]', 'rounded-[25px]', 'px-[20px]', 'py-[20px', 'border-[5px]', 'w-[20%]')
+            parent.appendChild(div)
+        });
         
     })
     .catch(error => console.error("Error:", error));
