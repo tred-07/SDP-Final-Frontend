fc8808cd63948feaa5b9897f3e4d5c5341bf9d67
tred-07 <aranna.rtx.bd.02@gmail.com>
2025-01-23 01:47:28 +0600

    update

--- profile.js
+++ profile.js
@@ -10,17 +10,40 @@ const fetchProfile=()=>{
         },
     ).
     then(response => response.json())
-    .then(data => {
+   .then(data => {
         data.forEach(element => {
-            const parent=document.getElementById("profile-container")
+            const parentsAdvertise=document.getElementById("myAdvertise")
+            parentsAdvertise.innerHTML=``
+            const parent=document.getElementById("profile-part")
             const div=document.createElement("div")
             div.innerHTML=`
-            <h2>Username: ${element.username}</h2>
-            <p>${element.first_name} ${element.last_name}</p>
-            <p>${element.email}</p>
+            <h2 class="text-center font-bold text-[2rem]">Username: ${element.username}</h2>
+            <p class="text-center font-bold text-[2rem]">Name: ${element.first_name} ${element.last_name}</p>
+            <p class="text-center font-bold text-[2rem]">Email: ${element.email}</p>
+            <h2 class="text-center font-bold text-[2rem]">Total Advertisements:${element.advertise.length}</h2>
+            <h2 class="text-center font-bold text-[2rem]">Total Favourite:${element.favourite.length}</h2>
+            <h2 class="text-center font-bold text-[2rem]">Total Advertisements:${element.request.length}</h2>
+            <h2 class="text-center font-bold text-[2rem]">Total Advertisements:${element.feedback.length}</h2>
             `
-            div.classList.add('flex', 'flex-col', 'justify-center', 'border-[black]', 'rounded-[25px]', 'px-[20px]', 'py-[20px', 'border-[5px]', 'w-[20%]')
+            div.classList.add('flex', 'flex-col', 'justify-center', 'border-[black]', 'rounded-[25px]', 'px-[20px]', 'py-[100px]', 'border-[5px]', 'w-[100%]')
             parent.appendChild(div)
+            console.log(element);
+            element.advertise.forEach(el=>{
+                console.log(el.id,el.title,el.description,el.price,el.image,el.is_approved,el.is_accepted,el.created_at,el.updated_at);
+            })
+
+            element.favourite.forEach(el=>{
+                console.log(el.id,el.advertise,el.created_at);
+            })
+            
+            element.request.forEach(el=>{
+                console.log(el.id,el.advertise,el.created_at);
+            })
+
+            element.feedback.forEach(el=>{
+                console.log(el.id,el.advertise,el.rating,el.comment,el.created_at);
+            })
+
         });
         
     })
